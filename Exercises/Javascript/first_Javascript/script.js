b9a4dd5aeef27f155003075384c24eab6cd6a6c5
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-01-16 19:52:51 +0100

    Create script.js

---
+++ Exercises/Javascript/first_Javascript/script.js
@@ -0,0 +1,28 @@
+  
+let name = 'Steven Koelma'
+console.log(name);
+
+
+let sum = 1 + 1;
+console.log (sum);
+
+let sumStrings = '4' + '4';
+console.log (sumStrings);
+
+let substraction = 99 - 10;
+console.log (substraction);
+
+let multification = 10 * 5;
+console.log (multification);
+
+let division = 100 / 4;
+console.log (division);
+
+let modulus = 100 % 3;
+console.log (modulus);
+
+let age = '29';
+console.log (typeof 29);
+
+let text = 'negenentwintig';
+console.log (typeof negenentwintig);
