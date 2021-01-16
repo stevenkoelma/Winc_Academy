96b87465cf2a85b867e1e6b630799d39f978dbc9
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-01-16 19:56:16 +0100

    Add files via upload

---
+++ Exercises/Javascript/If-else_statements/index.js
@@ -0,0 +1,33 @@
+
+const age = '22';
+const isFemale = true;
+const driverStatus = 'BoB';
+
+// leeftijd
+if (age >= 18) 
+{
+console.log ("U mag naar binnen!");
+} else {
+    console.log ("Sorry, u mag niet naar binnen");
+}
+
+// ladiesNight
+if (true)  
+{
+console.log ("Welkom op onze ladiesnight!");
+} else {
+    console.log ("Helaas, geen ladiesnight voor u");
+}
+
+// driverStatus
+if (driverStatus == 'BoB')
+{
+    console.log ("U mag zelf rijden")
+} else { 
+console.log ("U mag niet rijden")
+}
+
+
+
+
+
