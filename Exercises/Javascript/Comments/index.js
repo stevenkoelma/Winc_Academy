83c005fda5075693bbe7ada780944d8847957b57
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-01-16 19:54:33 +0100

    Add files via upload

---
+++ Exercises/Javascript/Comments/index.js
@@ -0,0 +1,9 @@
+console.log ("Hallo Winc Academy studenten")
+
+// Dit is een grote som
+/* let multification = 1230941 * 1823794;
+console.log (multification);
+
+let division = 637263 / 54 ;
+console.log (division);
+ */
