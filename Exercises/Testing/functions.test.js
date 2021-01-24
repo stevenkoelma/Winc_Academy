57fc23578da10076f59c075a3ff5c02433f8c53e
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-01-24 21:52:12 +0100

    Push to Github

---
+++ Exercises/Testing/functions.test.js
@@ -0,0 +1,29 @@
+const {addOne, getWordLengths, findNeedle } = require("./functions.js");
+
+test("Add 1 to each item in myArray", function() {
+  const myArray = [31, 57, 12, 5];
+
+  const unchanged = [31, 57, 12, 5];
+  const expected = [32, 58, 13, 6];
+  const output = addOne(myArray);
+
+  expect(output).toEqual(expected);
+  expect(myArray).toEqual(unchanged);
+});
+
+
+test("Get word lengths", function() {
+  const words = ["sun", "potato", "roundabout", "pizza"];
+  const expected = [3, 6, 10, 5];
+
+  const output = getWordLengths(words);
+  expect(output).toEqual(expected);
+});
+
+test("Find the needle", function() {
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
+
+  const output = findNeedle(words, "needle");
+  expect(output).toEqual(expected);
+});
