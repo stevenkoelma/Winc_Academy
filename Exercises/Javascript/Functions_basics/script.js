7c718dfe18757468f35d68c0d6f773981e7691b1
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-02-06 20:27:41 +0100

    Add files via upload

---
+++ Exercises/Javascript/Functions_basics/script.js
@@ -0,0 +1,17 @@
+const makePizza = () => {
+    console.log("Smeer tomatensaus op pizzadeeg")
+    console.log("Beleg pizza")
+    console.log("Stop in oven")
+}
+
+makePizza()
+makePizza()
+
+const makeSushi = () => {
+    console.log("Bereid rijst")
+    console.log("Bereid ingrediënten")
+    console.log("Maak de rolletjes")
+}
+makeSushi()
+makeSushi()
+makeSushi()
