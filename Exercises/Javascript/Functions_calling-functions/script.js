6e87e9d0d033b329828b842561ca1b04385f4579
Steven Koelma <67284329+stevenkoelma@users.noreply.github.com>
2021-02-10 09:29:29 +0100

    Update script.js

--- Exercises/Javascript/Functions_calling-functions/script.js
+++ Exercises/Javascript/Functions_calling-functions/script.js
@@ -2,20 +2,45 @@ const ageChecker = (age) => {
   if (age >= 18) {
     return true;
   } else {
-    console.log(false)
     return false;
-    
   }
 };
 
 const greet = (age) => {
- const adult = ageChecker(true)
- const kiddo = ageChecker(false)
-if (adult) {
-    console.log ("Hi there")
-    return
-}
+  if (ageChecker(age)) {
+    return "Hi there";
+  } else {
+    return "Hi kiddo";
+  }
 };
 
+console.log(greet(17));
+console.log(greet(18));
 
-console.log(greet(18))
+
+
+// baseprice and VAT
+// Return: price including VAT
+
+const calculateVAT = (baseprice, percentage) => baseprice * (percentage / 100)
+
+const calculatePriceWithVAT = (baseprice, percentage) => {
+  const VAT = calculateVAT(baseprice, percentage);
+  return baseprice + VAT;
+}
+
+console.log(calculatePriceWithVAT(1000, 21))
+console.log(calculatePriceWithVAT(1000, 9))
+console.log(calculatePriceWithVAT(1000, 0))
+
+
+const calculateBasePrice = (priceWithVAT, percentage) =>  priceWithVAT / ((100 + percentage) / 100)
+console.log(calculateBasePrice(1210, 21))
+
+const calculateBasePriceAndVAT = (priceWithVAT, percentage) => {
+  const basePrice = calculateBasePrice(priceWithVAT, percentage)
+  const VAT = priceWithVAT - basePrice
+ return [basePrice, VAT]
+}
+
+console.log(calculateBasePriceAndVAT(120, 21))
